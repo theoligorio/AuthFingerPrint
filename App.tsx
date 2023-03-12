b2a5c6c43f550404a2f58c2a5bbc2221647dac61
theoligorio <theoligorio04@gmail.com>
2023-03-12 00:13:15 -0300

    Autenticação com impressão digital e ronhecimento facial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { styles } from './styles';
+import React, { useState, useEffect } from 'react';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  // verificar se os dispositivos são compativeis para a authenticação
+  async function verifyAvaiableAuthentication() {
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    console.log(compatible)
+
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    console.log(types.map(type => LocalAuthentication.AuthenticationType[type]));
+  }
+
+  // função para autenticação
+  async function handleAuthentication () {
+    const isBiometricEnrolled = await LocalAuthentication.isEnrolledAsync();
+    // console.log(isBiometricEnrolled);
+
+  // função para se não houver biometria cadastrada
+    if(!isBiometricEnrolled) {
+      return Alert.alert('Login', 'Nenhuma biometria encontrada. Por favor, Cadastre no dispositivo')
+    }
+
+  // const para criar a autenticação com digital do usuário
+    const auth = await LocalAuthentication.authenticateAsync({
+      promptMessage: 'Login com Biometria',
+      fallbackLabel: 'Biometria não reconhecida'
+    });
+    // console.log(auth);
+
+    setIsAuthenticated(auth.success);
+  }
+
+  useEffect(() => {
+    verifyAvaiableAuthentication();
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        Usuário Conectado: { isAuthenticated ? 'Sim' : 'Não'}
+      </Text>
+
+      <Button 
+        title="entrar" onPress={handleAuthentication}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
